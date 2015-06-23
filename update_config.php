4c2e49aa2e95bc24e6df5dbb1bcaa1a935bc8d84
MusiqueLibre <dev@dogmazic.net>
2015-06-23 08:24:57 +0200

    ajout fichier de paramètres customs

---
+++ update_config.php
@@ -0,0 +1,22 @@
+<?php
+$fileText = file("./less/config.less") or die("Can't read les config file");
+$modifiedFileText = array_map('replaceLessProperty', $fileText);
+function replaceLessProperty($fileTextLine) {
+    //checker scope
+    $changedProperty = array(
+                                "@sans-fail" => 'bouh',
+                                "@serif-fail" => 'bla',
+                            );
+    $lineVariable = extractLessVariableOfLine($fileTextLine);
+    if(array_key_exists($lineVariable, $changedProperty)) {
+        echo $lineVariable;
+        return $lineVariable.": ".$changedProperty[$lineVariable].";\n";
+    } else {
+        return $fileTextLine;
+    }
+}
+function extractLessVariableOfLine($line) {
+    return trim(explode(':', $line)[0]);
+}
+file_put_contents('./less/config-custom.less', implode('', $modifiedFileText));
+

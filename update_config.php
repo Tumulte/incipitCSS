a443c93b7d6baa6eb579f4c36a8f7d984b3dba0c
tumulte <contact@tumulte.me>
2015-06-23 16:10:29 +0100

    settings as forms to be written in config

--- update_config.php
+++ update_config.php
@@ -1,22 +1,24 @@
 <?php
+var_dump($_POST);
 $fileText = file("./less/config.less") or die("Can't read les config file");
 $modifiedFileText = array_map('replaceLessProperty', $fileText);
-function replaceLessProperty($fileTextLine) {
+function replaceLessProperty($fileTextLine)
+{
     //checker scope
     $changedProperty = array(
                                 "@sans-fail" => 'bouh',
                                 "@serif-fail" => 'bla',
                             );
     $lineVariable = extractLessVariableOfLine($fileTextLine);
-    if(array_key_exists($lineVariable, $changedProperty)) {
+    if (array_key_exists($lineVariable, $changedProperty)) {
         echo $lineVariable;
         return $lineVariable.": ".$changedProperty[$lineVariable].";\n";
     } else {
         return $fileTextLine;
     }
 }
-function extractLessVariableOfLine($line) {
+function extractLessVariableOfLine($line)
+{
     return trim(explode(':', $line)[0]);
 }
 file_put_contents('./less/config-custom.less', implode('', $modifiedFileText));
-
